e1bba1b336526814311b8f26cb7f30052922e4d6
sergiynyk <128303103+sergiynyk@users.noreply.github.com>
2023-05-16 18:24:27 +0300

    Add files via upload

--- Final project/final_project.py
+++ Final project/final_project.py
@@ -4,35 +4,67 @@ import time as tm
 import os
 
 init()
+
 font.init()
+
 mixer.init()
 
 # розміри вікна
+test_image = None
+
+map_get = randint(3, 4)
+if map_get == 2:
+    test_image = "Maps/map2.png"
+elif map_get == 3:
+    test_image = "Maps/map3.png"
+elif map_get == 4:
+    test_image = "Maps/map4.png"
+
+
 WIDTH, HEIGHT = 900, 600
-playerwidth, playerheight = 75, 150
+
+playerstancewidth, playerstanceheight = 75, 150
+playerwalkingwidth, playerwalkingheight = 100, 153
+playerhitwidth, playerhitheight = 140, 150
 
 # картинка фону
-bg_image = image.load("background1.png")
-bg_imaget = transform.scale(bg_image, (1200, 600))
+bg_image = image.load(test_image)
+
+bg_imaget = transform.scale(bg_image, (900, 600))
+
 #картинки для спрайтів
-player_image = image.load("Animations/Stance/Stance1.png")
-ufo_image = image.load("ufo.png")
+
+hero_image_test = image.load("ufo.png")
 
 path = os.getcwd()
-stance_exp_images = os.listdir(path + '/Animations/Stance')
-walk_exp_images = os.listdir(path + '/Animations/Walking_Forward')
+
+stance_exp_images = os.listdir(path + '/SubzeroAnimations/Stance')
+walk_exp_images = os.listdir(path + '/SubzeroAnimations/Walking_Forward')
+hit_exp_images = os.listdir(path + '/SubzeroAnimations/PunchForward')
+
 walk_image_list = []
+
 stance_image_list = []
+
+hit_image_list = []
+
 for img in stance_exp_images:
-    stance_image_list.append(transform.scale(image.load('Animations/Stance/' + img), (playerwidth, playerheight)))
+    stance_image_list.append(transform.scale(image.load('SubzeroAnimations/Stance/' + img), (playerstancewidth, playerstanceheight)))
+
+
 for img in walk_exp_images:
-    walk_image_list.append(transform.scale(image.load('Animations/Walking_Forward/' + img), (playerwidth, playerheight)))
-#положення наразі:
+    walk_image_list.append(transform.scale(image.load('SubzeroAnimations/Walking_Forward/' + img), (playerwalkingwidth, playerwalkingheight)))
+
+
+for img in hit_exp_images:
+    hit_image_list.append(transform.scale(image.load('SubzeroAnimations/PunchForward/' + img), (playerhitwidth, playerhitheight)))
+
 
 
 # класи
+
 class GameSprite(sprite.Sprite):
-    def __init__(self, sprite_img, width, height, x, y, speed):
+    def __init__(self, sprite_img, width, height, x, y, speed, hp):
         super().__init__()
         self.image = transform.scale(sprite_img, (width, height))
         self.rect = self.image.get_rect()
@@ -40,36 +72,54 @@ class GameSprite(sprite.Sprite):
         self.rect.y = y
         self.speed = speed
         self.mask = mask.from_surface(self.image)
+        self.hp = hp
 
-        self.images = stance_image_list
-        self.k = 0
-        self.k2 = 0
-        self.frames = 0
-        self.frames2 = 0
-        self.sstill = 1
-        self.swalking = 0
 
     def draw(self): #відрисовуємо спрайт у вікні
         window.blit(self.image, self.rect)
 
 class Player(GameSprite):
+    def __init__(self, sprite_img, width, height, x, y, speed, hp):
+        super().__init__(sprite_img, width, height, x, y, speed, hp)
+        self.stance_images = stance_image_list
+        self.walking_images = walk_image_list
+        self.hit_images = hit_image_list
+        self.timer = 0
+        self.k = 0
+        self.k2 = 0
+        self.k3 = 0
+        self.frames = 0
+        self.frames2 = 0
+        self.frames3 = 0
+        self.sstill = 1
+        self.swalking = 0
+        self.sdhit = 0
     def update(self): #рух спрайту
         keys_pressed = key.get_pressed()
         if not keys_pressed[K_LEFT] and not keys_pressed[K_RIGHT]:
             self.sstill = 1
             self.swalking = 0
+            self.sdhit = 0
+        if keys_pressed[K_e] and not keys_pressed[K_LEFT] and not keys_pressed[K_RIGHT]:
+            self.sstill = 0
+            self.swalking = 0
+            self.sdhit = 1
         if keys_pressed[K_LEFT] and self.rect.x > 0:
             self.rect.x -= self.speed
             self.sstill = 0
             self.swalking = 1
+            self.sdhit = 0
         if keys_pressed[K_RIGHT] and self.rect.x < WIDTH - 70:
             self.rect.x += self.speed
             self.sstill = 0
             self.swalking = 1
+            self.sdhit = 0
         if self.sstill == 1:
             self.stand_animations()
         if self.swalking == 1:
             self.walking_animations()
+        if self.sdhit == 1:
+            self.hit_animations()
     def stand_animations(self):
         self.frames += 1
         if self.frames == 5:
@@ -77,7 +127,7 @@ class Player(GameSprite):
                 self.frames = 0
                 if self.k == len(stance_image_list):
                     self.k = 0
-                self.image = self.images[self.k]
+                self.image = self.stance_images[self.k]
     def walking_animations(self):
         self.frames2 += 1
         if self.frames2 == 5:
@@ -85,15 +135,19 @@ class Player(GameSprite):
                 self.frames2 = 0
                 if self.k2 == len(walk_image_list):
                     self.k2 = 0
-                self.image = self.images[self.k2]
-    
+                self.image = self.walking_images[self.k2]
+    def hit_animations(self):
+        self.timer = tm.time()
+        self.frames3 += 1
+        if self.frames3 == 5 and self.timer >= 3:
+            self.k3 += 1
+            self.frames3 = 0
+            if self.k3 == len(hit_image_list):
+                self.k3 = 0
+                self.timer = tm.time()
+            self.image = self.hit_images[self.k3]
 
 
-            
-        
-
-
-        
 class Text(sprite.Sprite):
     def __init__(self, text, x, y, font_size=22, font_name="Impact", color=(255,255,255)):
         self.font = font.SysFont(font_name, font_size)
@@ -109,23 +163,26 @@ class Text(sprite.Sprite):
     def set_text(self, new_text): #змінюємо текст напису
         self.image = self.font.render(new_text, True, self.color)
 
-
 # створення вікна
+
 window = display.set_mode((WIDTH, HEIGHT))
 display.set_caption("Шутер")
 
 # написи для лічильників очок
+
 score_text = Text("Рахунок: 0", 20, 50)
+
 # напис з результатом гри
+
 result_text = Text("Перемога!", 350, 250, font_size = 50)
 
-#додавання фону
-
 # створення спрайтів
-player = Player(player_image, width = playerwidth, height = playerheight, x = 200, y = HEIGHT-150, speed= 3)
+
+player = Player(hero_image_test, width = playerhitwidth, height = playerhitheight, x = 200, y = HEIGHT-225, speed= 3, hp = 100)
 
 
 # основні змінні для гри
+
 run = True
 finish = False
 clock = time.Clock()
@@ -134,6 +191,7 @@ score = 0
 lost = 0
 
 # ігровий цикл
+
 while run:
     # перевірка подій
     for e in event.get():
@@ -142,13 +200,19 @@ while run:
         if e.type == KEYDOWN:
             pass
     if not finish: # поки гра триває
-        window.blit(bg_imaget, (0, 0)) 
+        window.blit(bg_imaget, (0, 0))
         # рух спрайтів
+        if player.k3 == len(hit_image_list):
+            player.timer = tm.time()
         player.draw()
         player.update() #рух гравця
     else:
         result_text.draw() # текст вкінці гри
-    score_text.draw()
+
+
     # оновлення екрану і FPS
+
     display.update()
     clock.tick(FPS)
+
+    #transform.flip(image, True, False)
